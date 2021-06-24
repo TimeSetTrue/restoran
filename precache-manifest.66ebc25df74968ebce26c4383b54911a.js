adc8df4e0c960165e9fd87a96fd94a38ca7d5de0
Andrey <altus01@mail.ru>
2021-06-24 14:05:34 +0400

    Updates

---
+++ precache-manifest.66ebc25df74968ebce26c4383b54911a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4c439d0717c222dd733e41f0466e01",
+    "url": "/restoran/index.html"
+  },
+  {
+    "revision": "2fe746aa58fe60098989",
+    "url": "/restoran/static/css/main.08a030ce.chunk.css"
+  },
+  {
+    "revision": "9cc78d98c5cf92fa3759",
+    "url": "/restoran/static/js/2.503ab65d.chunk.js"
+  },
+  {
+    "revision": "2fe746aa58fe60098989",
+    "url": "/restoran/static/js/main.441025d6.chunk.js"
+  },
+  {
+    "revision": "45eee38cda0d603064e5",
+    "url": "/restoran/static/js/runtime~main.723cbc46.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/restoran/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/restoran/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
